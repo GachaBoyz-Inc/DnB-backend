6a253c2866182fc7e8f51427a8899ff844545b13
Brunoficial <bruno.lopes.vrls@gmail.com>
2026-01-19 20:52:50 -0300

    chore: Primeiro commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Olá Mundo!')
+})
+
+app.listen(port, () => {
+  console.log(`Exemplo de app rodando em http://localhost:${port}`)
+})
