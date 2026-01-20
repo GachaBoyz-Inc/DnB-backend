998f32af87ef7acb89432d2c0611aede3619b171
Brunoficial <bruno.lopes.vrls@gmail.com>
2026-01-19 21:42:38 -0300

    feat: Criado model de Skills, com seus getters e setters

---
+++ models/Skills.ts
@@ -0,0 +1,173 @@
+// These class represents a group of skills which a character has. The numbers define the bonus in each skill.
+
+class Skills {
+    private id: number 
+    private acrobatics: number 
+    private arcana: number 
+    private athletics: number 
+    private perfomance: number 
+    private deception: number 
+    private stealth: number 
+    private history: number 
+    private intimidation: number 
+    private insight: number 
+    private investigation: number 
+    private animal_handling: number 
+    private medicine: number 
+    private nature: number 
+    private perception: number 
+    private persuasion: number 
+    private sleight_of_hand: number 
+    private religion: number 
+    private survival: number 
+
+    getSkillsId() {
+        return this.id 
+    }
+
+    getSkillsAcrobatics() {
+        return this.acrobatics
+    }
+
+    getSkillsArcana() {
+        return this.arcana
+    }
+
+    getSkillsAthletics() {
+        return this.athletics
+    }
+
+    getSkillsPerformance() {
+        return this.perfomance
+    }
+
+    getSkillsDeception() {
+        return this.deception
+    }
+
+    getSkillsStealth() {
+        return this.stealth
+    }
+
+    getSkillsHistory() {
+        return this.history
+    }
+
+    getSkillsIntimidation() {
+        return this.intimidation
+    }
+
+    getSkillsInsight() {
+        return this.insight
+    }
+
+    getSkillsInvestigation() {
+        return this.investigation
+    }
+
+    getSkillsAnimalHandling() {
+        return this.animal_handling
+    }
+
+    getSkillsMedicine() {
+        return this.medicine
+    }
+
+    getSkillsNature() {
+        return this.nature
+    }
+
+    getSkillsPerception() {
+        return this.perception
+    }
+
+    getSkillsPersuasion() {
+        return this.persuasion
+    }
+
+    getSkillsSleightOfHand() {
+        return this.sleight_of_hand
+    }
+
+    getSkillsReligion() {
+        return this.religion
+    }
+
+    getSkillsSurvival() {
+        return this.survival
+    }
+
+    setSkillsAcrobatics(value:number) {
+        this.acrobatics = value
+    }
+
+    setSkillsArcana(value:number) {
+        this.arcana = value
+    }
+
+    setSkillsAthletics(value:number) {
+        this.athletics = value
+    }
+
+    setSkillsPerformance(value:number) {
+        this.perfomance = value
+    }
+
+    setSkillsDeception(value:number) {
+        this.deception = value
+    }
+
+    setSkillsStealth(value:number) {
+        this.stealth = value
+    }
+
+    setSkillsHistory(value:number) {
+        this.history = value
+    }
+
+    setSkillsIntimidation(value:number) {
+        this.intimidation = value
+    }
+
+    setSkillsInsight(value:number) {
+        this.insight = value
+    }
+
+    setSkillsInvestigation(value:number) {
+        this.investigation = value
+    }
+
+    setSkillsAnimalHandling(value:number) {
+        this.animal_handling = value
+    }
+
+    setSkillsMedicine(value:number) {
+        this.medicine = value
+    }
+
+    setSkillsNature(value:number) {
+        this.nature = value
+    }
+
+    setSkillsPerception(value:number) {
+        this.perception = value
+    }
+
+    setSkillsPersuasion(value:number) {
+        this.persuasion = value
+    }
+
+    setSkillsSleightOfHand(value:number) {
+        this.sleight_of_hand = value
+    }
+
+    setSkillsReligion(value:number) {
+        this.religion = value
+    }
+
+    setSkillsSurvival(value:number) {
+        this.survival = value
+    }
+} 
+
+export default Skills
