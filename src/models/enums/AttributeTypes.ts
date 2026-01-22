bfa3988c62b9df0aeee319243abbe5f56ae434d3
Bruno <bruno.lopes.vrls@gmail>
2026-01-22 14:30:15 -0300

    feat: Criado um enum para tipos de atributos

---
+++ src/models/enums/AttributeTypes.ts
@@ -0,0 +1,10 @@
+enum AttributeTypes {
+    STR = "Força",
+    DEX = "Destreza",
+    WIS = "Sabedoria",
+    INT = "Inteligência", 
+    CONS = "Constituição", 
+    CHAR = "Carisma"
+} 
+
+export default AttributeTypes
