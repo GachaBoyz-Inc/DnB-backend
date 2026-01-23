9418445c142eec066bb8cbb9ef0ca7763a9d9a85
Brunoficial <bruno.lopes.vrls@gmail.com>
2026-01-23 15:40:55 -0300

    fix: Corrigido erro na tipagem de conjurationClass no arquivo de SpellAttack

--- src/models/SpellAttack.ts
+++ src/models/SpellAttack.ts
@@ -1,4 +1,4 @@
-import Class from "./Class.js";
+import ClassNames from "../models/enums/ClassNames.js"
 import ComponentTypes from "./enums/ComponentTypes.js";
 import SpellTypes from "./enums/SpellTypes.js";
 import Spell from "./Spell.js";
@@ -6,7 +6,7 @@ import Spell from "./Spell.js";
 class SpellAttack extends Spell {
     damage: string;
 
-    constructor(id: number, name: string, description: string, componentTypes: ComponentTypes[], casting_time: string, duration: string, school: string, range: string, conjurationClass: Class[], spellType: SpellTypes, damage: string) {
+    constructor(id: number, name: string, description: string, componentTypes: ComponentTypes[], casting_time: string, duration: string, school: string, range: string, conjurationClass: ClassNames[], spellType: SpellTypes, damage: string) {
         super(id, name, description, componentTypes, casting_time, duration, school, range, conjurationClass, spellType);
         this.damage = damage;
     }
