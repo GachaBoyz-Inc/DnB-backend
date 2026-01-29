328f2f616ffe8f07c1e14fa7dbbd1e9bd75349f5
Brunoficial <bruno.lopes.vrls@gmail.com>
2026-01-29 19:47:27 -0300

    feat: Criado o arquivo sql com as informações padrões do banco de dados de classes

---
+++ src/migrations/defaultDataFromClasses.sql
@@ -0,0 +1,403 @@
+/*
+Classes pré-existences
+*/
+
+INSERT INTO class (name, description, hpDice, proficiencies, skills, equipment) 
+
+VALUES 
+("Bárbaro", 
+"Esses bárbaros, diferentes da forma que conseguem,
+são definidos por sua fúria: desenfreada, inextinguível e
+irracional fúria. Mais que uma mera emoção, sua raiva é
+a ferocidade de um predador acuado, o assalto implacável
+de uma tempestade, a turbulência agitada do mar.",
+
+"1d12", 
+
+"Armaduras: Armaduras leves, armaduras médias e
+escudos
+Armas: Armas simples, armas marciais
+Ferramentas: Nenhuma
+Testes de Resistência: Força, Constituição",
+
+"Escolha duas dentre Adestrar Animais,
+Atletismo, Intimidação, Natureza, Percepção e
+Sobrevivência",
+
+"Você começa com o seguinte equipamento, além do
+equipamento concedido pelo seu antecedente:
+(a) um machado grande ou (b) qualquer arma marcial
+corpo-a-corpo
+(a) dois machados de mão ou (b) qualquer arma
+simples
+Um pacote de aventureiro e quatro azagaias"
+), 
+
+(
+"Bardo",
+
+"Não importa se um escolar, escaldo ou malandro,
+o bardo tece sua magia através de palavras e
+música para inspirar aliados, desmoralizar
+oponentes, manipular mentes, criar ilusões e,
+até mesmo, curar ferimentos.", 
+
+"1d8",
+
+"Armaduras: Armaduras leves
+Armas: Armas simples, bestas de mão, espadas longas,
+rapieiras, espadas curtas
+Ferramentas: Três instrumentos musicais, à sua escolha
+Testes de Resistência: Destreza, Carisma", 
+
+"Escolha três quaisquer",
+
+"Você começa com o seguinte equipamento, além do
+equipamento concedido pelo seu antecedente:
+(a) uma rapieira, (b) uma espada longa ou (c) qualquer
+arma simples
+(a) um pacote de diplomata ou (b) um pacote de artista
+(a) um lute ou (b) qualquer outro instrumento musical
+Armadura de couro e uma adaga"
+),
+
+(
+   "Bruxo",
+
+   "Os bruxos são desbravadores do conhecimento que
+existe escondido no tecido do multiverso. Através de
+pactos feitos com seres misteriosos detentores de poder
+sobrenatural, os bruxos desbloqueiam efeitos mágicos tão
+sutis quanto espetaculares. Extraindo o conhecimento
+antigo de seres como nobres fadas, demônios, diabos,
+bruxas e entidades alienígenas do Reino Distante, os
+bruxos remontam segredos arcanos para aprimorar seus
+próprios poderes.",
+
+"1d8",
+
+"Armaduras: Armaduras leves
+Armas: Armas simples
+Ferramentas: Nenhuma
+Testes de Resistência: Sabedoria, Carisma",
+
+"Escolha duas entre Arcanismo, Enganação,
+História, Intimidação, Investigação, Natureza e
+Religião",
+
+"Você começa com o seguinte equipamento, além do
+equipamento concedido pelo seu antecedente:
+(a) uma besta leve e 20 virotes ou (b) qualquer arma
+simples
+(a) uma bolsa de componentes ou (b) um foco arcano
+(a) um pacote de estudioso ou (b) um pacote de
+explorador
+Armadura de couro, qualquer arma simples e duas
+adagas"
+),
+
+(
+   "Clérigo", 
+
+   "Clérigos são intermediadores entre o mundo mortal e o
+distante plano dos deuses. Tão variados quanto os deuses
+que servem, clérigos se esforçam para ser a própria mão
+de seus deuses. Não é apenas um sacerdote comum, mas
+alguém investido de poder divino.",
+
+"1d8",
+
+"Armaduras: Armaduras leves, armaduras médias,
+escudos
+Armas: Todas as armas simples
+Ferramentas: Nenhuma
+Testes de Resistência: Sabedoria, Carisma",
+
+"Escolha duas dentre História, Intuição,
+Medicina, Persuasão e Religião",
+
+"Você começa com o seguinte equipamento, além do
+equipamento concedido pelo seu antecedente:
+(a) uma maça ou (b) um martelo de guerra (se for
+proficiente)
+(a) brunea, (b) armadura de couro ou (c) cota de malha
+(se for proficiente)
+(a) um besta leve e 20 virotes ou (b) qualquer arma
+simples
+(a) um pacote de sacerdote ou (b) um pacote de
+aventureiro
+Um escudo e um símbolo sagrado"
+),
+
+(
+   "Druida",
+
+   "Quer seja convocando as forças elementais da
+natureza, ou emulando as criaturas do mundo animal, os
+druidas são encarnações da resistência, astúcia e fúria da
+natureza. Eles não se consideram donos da natureza. Ao
+invés disso, eles se veem como extensões da vontade
+indomável da natureza.",
+
+"1d8",
+
+"Armaduras: Armaduras leves, armaduras médias,
+escudos (druidas não irão vestir armaduras ou usar
+escudos feitos de metal)
+Armas: Clavas, adagas, dardos, azagaias, maças,
+bordões, cimitarras, foices, fundas e lanças.
+Ferramentas: Kit de herbalismo
+Testes de Resistência: Inteligência, Sabedoria",
+
+"Escolha duas dentre Arcanismo, Adestrar
+Animais, Intuição, Medicina, Natureza, Percepção,
+Religião e Sobrevivência",
+
+"Você começa com o seguinte equipamento, além do
+equipamento concedido pelo seu antecedente:
+(a) um escudo de madeira ou (b) qualquer arma
+simples
+(a) uma cimitarra ou (b) qualquer arma corpo-a-corpo
+simples
+(a) um pacote de estudioso ou (b) um pacote de
+explorador
+Armadura de couro, um pacote de aventureiro e um
+foco druídico"
+),
+
+(
+   "Feiticeiro",
+
+   "Os feiticeiros não veem serventia em grimórios ou
+antigos tomos de conhecimento místico buscados pelos
+magos, nem buscam um patrono para conceder
+-lhes suas
+magias, como um bruxo faz. Ao aprender a explorar e
+canalizar sua própria magia inata, eles descobrem novas
+e incríveis formas de liberar esse poder.",
+
+"1d6", 
+
+"Armaduras: Nenhuma
+Armas: Adagas, dardos, fundas, bordões e bestas leves
+Ferramentas: Nenhuma
+Testes de Resistência: Constituição, Carisma",
+
+"Escolha duas dentre Arcanismo, Enganação,
+Intuição, Intimidação, Persuasão e Religião",
+
+"Você começa com o seguinte equipamento, além do
+equipamento concedido pelo seu antecedente:
+(a) uma besta leve e 20 virotes ou (b) qualquer arma
+simples
+(a) uma bolsa de componentes ou (b) um foco arcano
+(a) um pacote de explorador ou (b) um pacote de
+aventureiro"
+),
+
+(
+   "Guerreiro",
+
+   "Cavaleiros em missões, lordes
+conquistadores, campeões reais, infantaria de elite,
+mercenários rígidos e bandidos reis, como guerreiros, eles
+compartilham de uma maestria com armas e armaduras
+sem precedentes, bem como um vasto conhecimento e
+habilidades em combate. E eles estão bem familiarizados
+com a morte, seja simplesmente conhecendo-a ou
+desafiando-a cara a cara.",
+
+"1d10",
+
+"Armaduras: Todas as armaduras, escudos
+Armas: Armas simples, armas marciais
+Ferramentas: nenhum
+Testes de Resistência: Força, Constituição",
+
+"Escolha duas dentre Acrobacia, Adestrar
+animais, Atletismo, História, Intuição, Intimidação,
+Percepção e Sobrevivência",
+
+"Você começa com o seguinte equipamento, além do
+equipamento concedido pelo seu antecedente:
+(a) cota de malha ou (b) gibão de peles, arco longo e 20
+flechas
+(a) uma arma marcial e um escudo ou (b) duas armas
+marciais
+(a) uma besta leve e 20 virotes ou (b) dois machados de
+arremesso
+(a) uma pacote de aventureiro ou (b) um pacote de
+explorador"
+), 
+
+(
+   "Ladino",
+
+   "Ladinos contam com sua perícia, furtividade e as
+vulnerabilidades de seus inimigos para obter vantagem
+em qualquer situação. Eles possuem uma habilidade
+especial para encontrar a solução para praticamente
+qualquer problema, demonstrando desenvoltura e
+versatilidade, a chave de qualquer grupo aventureiro de
+sucesso.",
+
+"1d8",
+
+"Armaduras: Armaduras leves
+Armas: Armas simples, bestas de mão, espadas longas,
+rapieiras, espadas curtas
+Ferramentas: Ferramentas de ladrão
+Testes de Resistência: Destreza, Inteligência",
+
+"Escolha quatro dentre Acrobacia, Atletismo,
+Atuação, Enganação, Furtividade, Intimidação,
+Intuição, Investigação, Percepção, Persuasão e
+Prestidigitação",
+
+"Você começa com o seguinte equipamento, além do
+equipamento concedido pelo seu antecedente:
+(a) uma rapieira ou (b) uma espada longa
+(a) um arco curto e uma aljava com 20 flechas ou (b)
+uma espada curta
+(a) um pacote de assaltante ou (b) um pacote de
+aventureiro ou (c) um pacote de explorador
+Armadura de couro, duas adagas e ferramentas de
+ladrão"
+),
+
+(
+   "Mago",
+
+   "Os magos são usuários de magia soberanos, unidos e
+definidos como uma classe pelas magias que conjuram.
+Usufruindo de uma trama sutil de magia que permeia o
+cosmos, os magos conjuram magias explosivas de fogo,
+arcos de relâmpagos, enganos sutis e controle de mentes
+de força bruta. Sua magia invoca monstros de outros
+planos de existência, vislumbra o futuro ou transforma
+inimigos mortos em zumbis. Suas magias mais poderosas
+podem transformar uma substância em outra, evocar
+meteoros que caem do céu ou abrir portais para outros
+mundos.",
+
+"1d6",
+
+"Armaduras: Nenhuma
+Armas: Adagas, dardos, fundas, bordões, bestas leves
+Ferramentas: Nenhuma
+Testes de Resistência: Inteligência, Sabedoria",
+
+"Escolha duas entre Arcanismo, História,
+Intuição, Investigação, Medicina e Religião",
+
+"Você começa com o seguinte equipamento, além do
+equipamento concedido pelo seu antecedente:
+(a) um bordão ou (b) uma adaga
+(a) uma bolsa de componentes ou (b) um foco arcano
+(a) um pacote de estudioso ou (b) um pacote de
+explorador
+Um grimório"
+),
+
+(
+   "Monge",
+
+   "Qualquer que seja suas disciplinas, os monges partilha
+de sua habilidade de aproveitar magicamente da energia
+que flui em seus corpos. Se canalizada como um exibição
+impressionante de maestria do combate, ou como um foco
+mais sutil de habilidade defensiva e velocidade, essa
+energia infunde tudo que um monge faz.",
+
+"1d8",
+
+"Armaduras: Nenhuma
+Armas: Armas simples, espadas curtas
+Ferramentas: Escolha um tipo de ferramenta de artesão
+ou um instrumento musical
+Testes de Resistência: Força, Destreza",
+
+"Escolha duas dentre Acrobacia, Atletismo,
+Furtividade, História, Intuição e Religião",
+
+"Você começa com o seguinte equipamento, além do
+equipamento concedido pelo seu antecedente:
+(a) uma espada curta ou (b) qualquer arma simples
+(a) um pacote de explorador ou (b) um pacote de
+aventureiro
+10 dardos"
+),
+
+(
+   "Paladino",
+
+   "Seja lá quais forem suas origens e suas missões,
+paladinos são unidos pelos seus juramentos de se
+imporem contra as forças do mal. Quer seja jurado ante o
+altar de um deus com um sacerdote como testemunha,
+quer seja em um clareira sagrada diante dos espíritos da
+natureza e seres feéricos, ou em um momento de
+desespero e aflição com os mortos como únicas
+testemunhas, o juramento de um paladino é um laço
+poderoso. Ele é uma fonte de poder que transforma um
+guerreiro devotado em um campeão abençoado.",
+
+"1d10",
+
+"Armaduras: Todas as armaduras, escudos
+Armas: Armas simples, armas marciais
+Ferramentas: Nenhum
+Testes de Resistência: Sabedoria, Carisma",
+
+"Escolha duas dentre Atletismo, Intuição,
+Intimidação, Medicina, Persuasão e Religião",
+
+"Você começa com o seguinte equipamento, além do
+equipamento concedido pelo seu antecedente:
+(a) uma arma marcial e um escudo ou (b) duas armas
+marciais
+(a) cinco azagaias ou (b) qualquer arma simples corpo-
+a-corpo
+(a) um pacote de sacerdote ou (b) um pacote de
+aventureiro
+Cota de malha e um símbolo sagrado"
+),
+
+(
+   "Patrulheiro",
+
+   "Guerreiros da natureza, os patrulheiros se especializaram
+em caçar monstros que ameaçam as margens da
+civilização
+– assaltantes humanoides, bestas e
+monstruosidades devastadoras, gigantes terríveis e
+dragões mortais. Eles aprendem a rastrear suas
+presas como os predadores fazem, movendo
+-se
+silenciosamente nas florestas e se escondendo atrás
+de arbustos e pedregulhos. Os patrulheiros focam seu
+treinamento de combate em técnicas que sejam
+especialmente úteis contra seus inimigos favoritos
+específicos.",
+
+"1d10",
+
+"Armaduras: Armaduras leves, armaduras médias,
+escudos
+Armas: Armas simples, armas marciais
+Ferramentas: Nenhuma
+Testes de Resistência: Força, Destreza",
+
+"Escolha três dentre Acrobacia, Adestrar
+Animais, Atletismo, Furtividade, Intuição,
+Investigação, Natureza, Percepção e Sobrevivência",
+
+"Você começa com o seguinte equipamento, além do
+equipamento concedido pelo seu antecedente:
+(a) camisão de malha ou (b) armadura de couro
+(a) duas espadas curtas ou (b) duas armas simples
+corpo-a-corpo
+(a) um pacote de explorador ou (b) um pacote de
+aventureiro
+Um arco longo e uma aljava com 20 flechas"
+)
