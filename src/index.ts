4c6cca7150b780defab946b3ef008e0661bb62a8
Brunoficial <bruno.lopes.vrls@gmail.com>
2026-01-29 19:00:27 -0300

    fix: Adicionado ao index rotas que estavam ausentes

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import racesRouter from "./router/RaceRouter.js"
 import classesRouter from "./router/ClassRouter.js"
 import authRouter from "./router/AuthRouter.js"
 import abilityRouter from "./router/AbilityRouter.js";
+import skillsRouter from "./router/SkillsRouter.js"
+import spellsRouter from "./router/SpellRouter.js"
+import itemsRouter from "./router/ItemRouter.js"
 import app from "./server/server.js";
 
 app.use("/users", userRouter);
@@ -16,6 +19,9 @@ app.use("/attributes", attributesRouter)
 app.use("/classes", classesRouter)
 app.use("/auth", authRouter)
 app.use("/abilities", abilityRouter);
+app.use("/skills", skillsRouter)
+app.use("/spells", spellsRouter)
+app.use("/items", itemsRouter)
 
 app.get('/', async (req, res) => {
   res.send('API is running');
---
+++ src/migrations/defaultData.sql
@@ -0,0 +1,12 @@
+/*
+Classes pré-existences
+*/
+
+INSERT INTO class (name, description, hpDice, proficiencies, skills, equipment) 
+VALUES ("Guerreiro")
+   name varchar(60) NOT NULL,
+    description varchar(1000) NOT NULL,
+    hpDice varchar(8) NOT NULL,
+    proficiencies varchar(600),
+    skills varchar(1000) NOT NULL,
+    equipment varchar(600) NOT NULL,
