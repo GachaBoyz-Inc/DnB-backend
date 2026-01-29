d403003db22f13ee5ffaae500a554d631f4df3b2
Brunoficial <bruno.lopes.vrls@gmail.com>
2026-01-28 22:30:18 -0300

    fix: Consertado as rotas não registradas em index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import backgroundRouter from "./router/BackgroundRouter.js";
 import userRouter from "./router/UserRouter.js";
 import characterRouter from "./router/CharacterRouter.js"
+import attributesRouter from "./router/AttributesRouter.js"
+import racesRouter from "./router/RaceRouter.js"
+import classesRouter from "./router/ClassRouter.js"
 import app from "./server/server.js";
 
 app.use("/users", userRouter);
 app.use("/backgrounds", backgroundRouter)
 app.use("/races", racesRouter)
+app.use("/characters", characterRouter)
 app.use("/attributes", attributesRouter)
+app.use("/classes", classesRouter)
 
 app.get('/', async (req, res) => {
   res.send('API is running');
